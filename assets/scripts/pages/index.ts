98c10a9300d1a1b382ff074a140039dece6c1477
samjmckenzie <25828757+samjmckenzie@users.noreply.github.com>
2020-07-23 20:56:22 +0200

    Add slightly more white space between projects, fixed border for mobile devices, fixed about link

--- assets/scripts/pages/index.ts
+++ assets/scripts/pages/index.ts
@@ -1,16 +1,20 @@
 import '../modules/smooth-scroll';
 
-const headerElement = document.getElementsByTagName('header')[0]
-const observer = new IntersectionObserver(entries => {
-    for(const entry of entries) {
-        if(!entry.isIntersecting) {
-            headerElement.classList.add('show-border');
-        } else {
-            headerElement.classList.remove('show-border');
-        }
-    }
-}, {
-    threshold: 1
-});
+let showBorderFixed = false;
 
-observer.observe(document.querySelector('section.introduction div.container'));
+const headerElement = document.getElementsByTagName('header')[0]
+if("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver(entries => {
+        for(const entry of entries) {
+            if(entry.isIntersecting) {
+                headerElement.classList.add('hide-border');
+            } else {
+                headerElement.classList.remove('hide-border');
+            }
+        }
+    }, {
+        threshold: 1,
+    });
+
+    observer.observe(document.querySelector('section.about div.container'));
+}

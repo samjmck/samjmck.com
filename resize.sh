5a20c5774e103f5f6fdc2ae566d2e43bb735dada
samjmckenzie <25828757+samjmckenzie@users.noreply.github.com>
2020-07-27 20:33:28 +0200

    Change image sizes for Lighthouse 100 performance score

--- resize.sh
+++ resize.sh
@@ -1,7 +1,7 @@
-convert "$1/$2".png -resize 900 -quality 60 "$1/$2_900w".png
-convert "$1/$2".png -resize 1400 -quality 60 "$1/$2_1400w".png
-convert "$1/$2".png -resize 1700 -quality 60 "$1/$2_1700w".png
+convert "$1/$2".png -resize 700 -quality 60 "$1/$2_700w".png
+convert "$1/$2".png -resize 1100 -quality 60 "$1/$2_1100w".png
+convert "$1/$2".png -resize 1450 -quality 60 "$1/$2_1450w".png
 
-convert "$1/$2".png -resize 900 -quality 95 "$1/$2_900w".webp
-convert "$1/$2".png -resize 1400 -quality 95 "$1/$2_1400w".webp
-convert "$1/$2".png -resize 1700 -quality 95 "$1/$2_1700w".webp
+convert "$1/$2".png -resize 700 -quality 95 "$1/$2_700w".webp
+convert "$1/$2".png -resize 1100 -quality 95 "$1/$2_1100w".webp
+convert "$1/$2".png -resize 1450 -quality 95 "$1/$2_1450w".webp
